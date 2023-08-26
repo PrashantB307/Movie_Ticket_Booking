f2e944d3e54fa638cd85f29f46a051bff2227cd0
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-08-26 22:40:25 +0530

    26-8-2023

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
 
+// Step 2 :- Add Event Listener
+
+movieSelect.addEventListener("change", e => {
+
+    ticketPrice = +e.target.value;
+
+    setMovieData(e.target.selectedIndex, e.target.value);
+
+    updateSelectedCount();
+
+});  
+
+container.addEventListener("click", e => {
+    if(e.target.classList.contains("seat") && !e.target.classList.contains("sold")){
+        e.target.classList.toggle("selected");
+
+        updateSelectedCount();
+    }
+});
+
