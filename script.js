9d93ce280c8805f074e72298de2da88e37e8e274
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-08-26 22:40:08 +0530

    26-8-2023

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// Step 1 :- Get references to DOM elements
+
+const container = document.querySelector(".container");
+
+const seats = document.querySelectorAll(".row .seat:not(.sold)");
+
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+
+const movieSelect = document.getElementById("movie");
+
+
